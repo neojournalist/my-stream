cba7267e881fbd1c4daafb4ae1266d0130fa8c2c
altynai <mambetova.altynai@gmail.com>
2024-02-27 12:08:53 +0100

    update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # Standard imports
-import pandas as pd
+#import pandas as pd
 
 # matplotlib
 #import matplotlib as mpl
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 #plotly
-import plotly.express as px
-import plotly.graph_objects as go
+#import plotly.express as px
+#import plotly.graph_objects as go
 
 import streamlit as st
 
